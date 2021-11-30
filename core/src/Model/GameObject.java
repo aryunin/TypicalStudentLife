a1e75a2f6b7b34d600772c6065f1f56f99fd22b2
Артем <ar.yunin@mail.ru>
2021-11-30 12:46:33 +0300

    GameObject added

---
+++ core/src/Model/GameObject.java
@@ -0,0 +1,33 @@
+package Model;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Polygon;
+
+import java.awt.geom.Point2D;
+
+abstract public class GameObject {
+    Sprite sprite;
+    Polygon bounds;
+
+    public GameObject(TextureRegion region, Point2D pos, float width, float height, float angle) {
+        bounds = new Polygon();
+        bounds.setVertices(new float[]{0f, 0f, width, 0f, width, height, 0f, height});
+        bounds.setPosition((float) pos.getX(), (float) pos.getY());
+        bounds.setOrigin(width/2,height/2);
+        bounds.setRotation(angle);
+
+        sprite = new Sprite(region);
+        sprite.setPosition((float) pos.getX(), (float) pos.getY());
+        sprite.setSize(width, height);
+        sprite.setOrigin(width/2,height/2);
+        sprite.setRotation(angle);
+    }
+
+    public void draw(SpriteBatch batch) {
+        sprite.setPosition(bounds.getX(), bounds.getY());
+        sprite.setRotation(bounds.getRotation());
+        sprite.draw(batch);
+    }
+}
