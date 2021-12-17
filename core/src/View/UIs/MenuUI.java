ca8e93ea65bd8a4819b00c63a43ee59fc5385057
Artemiiik <ar.yunin@mail.ru>
2021-12-18 01:15:01 +0300

    some changes

--- core/src/View/UIs/MenuUI.java
+++ core/src/View/UIs/MenuUI.java
@@ -2,14 +2,12 @@ package View.UIs;
 
 import Tools.ScreenManager;
 import View.Screens.GameScreen;
-import View.Screens.MenuScreen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MenuUI extends UI {
@@ -17,11 +15,12 @@ public class MenuUI extends UI {
 
     public MenuUI(Viewport viewport) {
         super(viewport, new Skin(Gdx.files.internal("glassy-ui.json")));
+
         playButton = new TextButton("Play",skin,"default");
         float playButtonWidth = 200;
         float playButtonHeight = 100;
         playButton.setSize(playButtonWidth,playButtonHeight);
-        playButton.setPosition(Gdx.graphics.getWidth() / 2 - playButtonWidth, Gdx.graphics.getHeight() / 2 - playButtonHeight);
+        playButton.setPosition(Gdx.graphics.getWidth() / 2f - playButtonWidth / 2f, Gdx.graphics.getHeight() / 2f - playButtonHeight / 2f);
         stage.addActor(playButton);
 
         playButton.addListener(new InputListener() {
